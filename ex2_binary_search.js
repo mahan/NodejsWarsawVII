4f3bc33b39b6460926ea4138e15f4ccb8cd80801
Mattias Hansson <mattias@techserio.com>
2022-03-15 22:17:21 +0100

    Formatted some whitespace

--- ex2_binary_search.js
+++ ex2_binary_search.js
@@ -5,23 +5,18 @@ import {performance} from 'perf_hooks';
 let binary_search = function (arr, x) {
 
     let start=0, end=arr.length-1;
-
     // Iterate while start not meets end
     while (start<=end){
-
         // Find the middle index
         let mid=Math.floor((start + end)/2);
-
         // If element is present at mid, return True
         if (arr[mid]===x) return mid;
-
         // Else look in left or right half accordingly
         else if (arr[mid] < x)
             start = mid + 1;
         else
             end = mid - 1;
     }
-
     return -1;
 }
 
